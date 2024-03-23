0174c3ddb19f2f4125c5852ca97e1d134e949301
GerardoMejia1107 <00104923@uca.edu.sv>
2024-03-23 14:17:38 -0600

    Se creo la clase Shape

---
+++ Shape.java
@@ -0,0 +1,24 @@
+public class Shape {
+    private double height;
+    private double base;
+
+    public double CalcArea(){
+        return 0;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
